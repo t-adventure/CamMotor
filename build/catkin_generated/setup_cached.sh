5ac96fdd8f103ab8fe1af008e9b0deda456f5784
t-adventure <trf.adventure@gmail.com>
2020-07-03 08:27:00 +0000

    adding

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vagrant/Workspace/ros/CamMotor/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/vagrant/Workspace/ros/CamMotor/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/vagrant/bin:/home/vagrant/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/vagrant/Workspace/ros/CamMotor/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vagrant/Workspace/ros/CamMotor/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/vagrant/Workspace/ros/CamMotor/src:$ROS_PACKAGE_PATH"
